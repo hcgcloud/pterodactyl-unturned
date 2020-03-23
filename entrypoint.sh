e8278111b803bd310a0f3b00a7e0b2db599cbf17
TLingC <5617150+TLingC@users.noreply.github.com>
2020-03-23 11:19:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,12 @@ if [ "${GAME_AUTOUPDATE}" == "1" ]; then
     ./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +login anonymous +force_install_dir /home/container +app_update 1110390 +quit
 fi
 
-cp -r /home/container/Extras/Rocket.Unturned /home/container/Modules/Rocket.Unturned
+cp -r Extras/Rocket.Unturned Modules/Rocket.Unturned
 
-cp -f /home/container/linux64/steamclient.so /home/container/Unturned_Headless_Data/Plugins/x86_64/steamclient.so
+cp -f linux64/steamclient.so Unturned_Headless_Data/Plugins/x86_64/steamclient.so
 
 ulimit -n 2048
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Unturned_Headless_Data/Plugins/x86_64/
-export TERM=xterm
 
 MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 echo ":/home/container$ ${MODIFIED_STARTUP}"
